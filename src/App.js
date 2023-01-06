0068380d7afc3a45abd52fe889d30aad767e13d6
BroskiVankis <ivanov.vankis@gmail.com>
2023-01-06 16:09:54 +0200

    First commit of the portfolio App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Projects from './Projects';
+import SocialProfiles from "./SocialProfiles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+    state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  }
+
+  render() {
+    return (
+    <div>
+     <h1>Hello!</h1>
+     <p>My name is Ivan. I'm a software engineer.</p>
+     <p>I'm always looking forward to working on a meaningful projects.</p>
+        {
+            this.state.displayBio ?  (
+                <div>
+                     <p>I live in Plovdiv, Bulgaria, and code every day.</p>
+                     <p>My favorite language is JavaScript, and i think React.js is awesome</p>
+                     <p>Besides coding, I also love music and Basketball!</p>
+                     <button onClick={this.toggleDisplayBio}>Show less</button>
+                </div>
+                ) : (
+                    <div>
+                        <button onClick={this.toggleDisplayBio}>Read more</button>
+                    </div>
+                    )
+        }
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id:1,
+        link: 'mailto:ivanov.vankis@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/BroskiVankis',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/ivan-ivanov-a50731185/',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        link: 'https://twitter.com/The3rdDegreeAI',
+        image: twitterIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
